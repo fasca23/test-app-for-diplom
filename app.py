f1b822566a91ea0ea576d547496dde055d3cdf51
Kamenev Vladimir <fasca23@yandex.ru>
2025-06-10 20:08:05 +0300

    Что-то поменял в коде8...

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ VERSION = get_version()
 
 @app.route('/')
 def hello():
-    return "Привет из Flask в Кубере! Каменев В.В. Версия: {VERSION}"
+    return "Привет из Flask в Кубере!!!!! Каменев В.В. Версия: {VERSION}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
