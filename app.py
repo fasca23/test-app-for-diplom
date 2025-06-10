caaaaa06f69fc6e2699bb4b40c216c2720cdd0c7
Kamenev Vladimir <fasca23@yandex.ru>
2025-06-10 14:22:51 +0300

    v.1.0.0

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Привет из Flask в Кубере! Каменев В.В."
+    return "Привет из Flask в Кубере! Каменев В.В. (v.1.0.0)"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
